94860883eb7e42e38d062002fffb08123cbc9c04
Kent Fredric <kentfredric@gmail.com>
2014-05-26 07:48:04 +1200

    Initial incarnation (No tests, no bootstrap)

--- lib/Dist/Zilla/Plugin/RewriteVersion/Sanitized.pm
+++ lib/Dist/Zilla/Plugin/RewriteVersion/Sanitized.pm
@@ -1,11 +1,32 @@
+use 5.008;    # utf8
 use strict;
 use warnings;
+use utf8;
 
 package Dist::Zilla::Plugin::RewriteVersion::Sanitized;
 
-# ABSTRACT:
+# ABSTRACT: RewriteVersion but force normalizing ENV{V} and other sources.
 
-use Moose;
+# AUTHORITY
+
+use Moose qw( extends with );
+
+=head1 SYNOPSIS
+
+As per C<[Git::NextVersion::Sanitized]|Dist::Zilla::Plugin::Git::NextVersion::Sanitized>
+
+  V=2.6.0 dzil release # -> V=2.006000 interally
+
+This is really just a glue layer that wraps L<< C<Dist::Zilla::Role::Version::Sanitize>|Dist::Zilla::Role::Version::Sanitize >>
+around L<< C<[RewriteVersion]>|Dist::Zilla::Plugin::RewriteVersion >>.
+
+So see L<< the documentation for Dist::Zilla::Role::Version::Sanitize|Dist::Zilla::Role::Version::Sanitize >> for details and
+attributes.
+
+=cut
+
+extends 'Dist::Zilla::Plugin::RewriteVersion';
+with 'Dist::Zilla::Role::Version::Sanitize';
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
