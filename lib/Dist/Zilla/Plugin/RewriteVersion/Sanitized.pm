58aa15976e0a4bd987aa625fe8d00e99ae0fb83b
Kent Fredric <kentfredric@gmail.com>
2015-01-14 23:09:47 +1300

    Build results of ef1ecd1 (on master)

--- lib/Dist/Zilla/Plugin/RewriteVersion/Sanitized.pm
+++ lib/Dist/Zilla/Plugin/RewriteVersion/Sanitized.pm
@@ -38,8 +38,8 @@ version 0.001005
 =head1 DESCRIPTION
 
 This is a subclass of L<< C<[RewriteVersion]>|Dist::Zilla::Plugin::RewriteVersion >> that applies version
-sanitization from all the various possible input sources 
-( Similar to L<< C<[Git::NextVersion::Sanitized]>|Dist::Zilla::Plugin::Git::NextVersion::Sanitized >> ) 
+sanitization from all the various possible input sources
+( Similar to L<< C<[Git::NextVersion::Sanitized]>|Dist::Zilla::Plugin::Git::NextVersion::Sanitized >> )
 by applying L<< C<Dist::Zilla::Role::Version::Sanitize>|Dist::Zilla::Role::Version::Sanitize >> to it.
 
 Using this module instead of C<[RewriteVersion]> allows you to do
@@ -48,7 +48,7 @@ Using this module instead of C<[RewriteVersion]> allows you to do
 
 And V will be interpreted as if you'd written C<V=2.006000>
 
-For details on the parameters this C<plugin> takes, 
+For details on the parameters this C<plugin> takes,
 see L<< the documentation for Dist::Zilla::Role::Version::Sanitize|Dist::Zilla::Role::Version::Sanitize >>.
 
 =head1 SEE ALSO
